0fbeded1002da37e18af0221b51e5ef5842ed483
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-01-01 12:07:10 +0900

    Create MemoryMemberRepository.java

---
+++ 실습/Repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package com.example.springtest.repository;
+import com.example.springtest.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setID(++sequence);
+        store.put(member.getID(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
