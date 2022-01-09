cf266d3ff9d6e070abfa699ee90c0ca43beab4bf
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2022-01-09 15:23:25 +0900

    Update MemberServiceImpl.java

--- 실습/With Java Code/MemberServiceImpl.java
+++ 실습/With Java Code/MemberServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // DIP 위반! 좋지 않은 코드이다.
 
     @Override
     public void join(Member member) {
